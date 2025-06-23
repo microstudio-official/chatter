c037c14c4e5f4291d2b6a39e81ef037a5486c327
Microstudio Official <hello@microstudio.one>
2025-06-22 20:56:22 -0500

    rm frontend for now

--- frontend/src/components/chat/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './ChatHeader';
-export * from './ChatSidebar';
-export * from './CreateRoomModal';
-export * from './MessageInput';
-export * from './MessageList';
-export * from './UserPanel';
