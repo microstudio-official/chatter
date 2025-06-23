030ad063bd33ae7fa84bb2c66f3537c1f3e55173
Microstudio Official <hello@microstudio.one>
2025-06-23 09:42:23 -0500

    add button component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -6,12 +7,9 @@ function App() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-lg font-bold">Vite + React</h1>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={() => setCount((count) => count + 1)}
-      >
+      <Button variant="default" onClick={() => setCount((count) => count + 1)}>
         count is {count}
-      </button>
+      </Button>
     </main>
   );
 }
