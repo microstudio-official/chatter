2437b2aa3f65c4cb40f3b3d701e3585a7c89ea9f
Microstudio Official <hello@microstudio.one>
2025-06-23 11:03:58 -0500

    add card, input, and label, with login form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from "react";
-import { Button } from "./components/ui/button";
+import { LoginComponent } from "./components/Login";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-lg font-bold">Vite + React</h1>
-      <Button variant="default" onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </Button>
+      <LoginComponent />
     </main>
   );
 }
--- frontend/src/components/ui/button.jsx
+++ frontend/src/components/ui/button.jsx
@@ -1,6 +1,5 @@
 import { Slot } from "@radix-ui/react-slot";
 import { cva } from "class-variance-authority";
-
 import { cn } from "../../lib/utils";
 
 const buttonVariants = cva(
